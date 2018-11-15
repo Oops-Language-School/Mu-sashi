b7a21ed8969f5206af5fabbde9ed98608d0a4fb5
peter <peter@CC-Peter.localdomain>
2018-11-15 15:47:08 -0800

    chatbot rearrange test v1

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,9 @@ router.get('/', (req, res, next) => {
     //console.log("REQ.USER: "+ req.user);
     if(req.user){
     //res.render('chat', { user: req.user, messages })
-        /*res.render('/chatbot/index',{
+        res.render('/chatbot/index',{
             title:"武藏後台一覽表",
-        });*/
-        res.render('./admin-lte/index');
+        });
     } else {
         res.redirect('/admin/login')
     }
