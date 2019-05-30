8bb0e2831b2584f77b1c143a2728158e41faf2b3
coer7 <wl00739514@gmail.com>
2019-05-29 18:37:52 -0700

    admin nav test

--- routes/oops-admin.js
+++ routes/oops-admin.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.render('./admin-lte/index');
 });
+router.get('/nav', (req, res, next) => {
+    res.render('./admin-lte/nav_top');
+});
 
 
 module.exports = router;
