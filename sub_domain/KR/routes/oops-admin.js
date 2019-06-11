38c8934220f29b9b91ad3e5cd7a3a4eb955c9996
PCHG <wl00739514@gmail.com>
2019-06-10 22:16:05 -0700

    kr admin set up

--- sub_domain/KR/routes/oops-admin.js
+++ sub_domain/KR/routes/oops-admin.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    res.render('./admin-lte/index');
+    res.render('./admin/KR/index');
 });
-
+router.get('/sample', (req, res, next) => {
+    res.render('./admin/KR/sample');
+});
+router.get('/blank', (req, res, next) => {
+    res.render('./admin/KR/blank_page');
+});
 
 module.exports = router;
